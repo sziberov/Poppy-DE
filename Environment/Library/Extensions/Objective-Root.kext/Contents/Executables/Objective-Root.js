3309a1413796a39278a6ebdca66a25e49eac1f90
DIES <sziberov@gmail.com>
2020-10-28 03:09:55 +0300

    Update

--- Environment/Library/Extensions/Objective-Root.kext/Contents/Executables/Objective-Root.js
+++ Environment/Library/Extensions/Objective-Root.kext/Contents/Executables/Objective-Root.js
@@ -1,3 +1,18 @@
+window.Math.randomArbitrary = (min, max) => {
+	return Math.random()*(max-min)+min;
+}
+window.Math.randomInt = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+
+	return Math.floor(Math.random()*(max-min))+min;
+}
+window.Math.randomIntInclusive = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+
+	return Math.floor(Math.random()*(max-min+1))+min;
+}
 window.nil = undefined;
 window.YES = true;
 window.NO = false;
