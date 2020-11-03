549bd9e602c7768268eede1d701cbfceea6ec851
DIES <sziberov@gmail.com>
2020-11-03 04:28:41 +0300

    Update

--- Environment/Library/Frameworks/CoreGraphics.framework/Contents/Modules/CoreGraphics.js
+++ Environment/Library/Frameworks/CoreGraphics.framework/Contents/Modules/CoreGraphics.js
@@ -5,6 +5,7 @@
 
 _import('CoreFoundation');
 
+_import('@Title', 'CGLayer');
 _import('@Title', 'CGAppearance');
 _import('@Title', 'CGElement');
 _import('@Title', 'CGCursor');
@@ -12,6 +13,7 @@ _import('@Title', 'CGFontManager');
 
 new CFProcessInfo().environment._CGAppearance = new CGAppearance('@Resources/Appearance.css').add();
 
+_CFShared.CGLayer = new CGLayer(_request('screen').width, _request('screen').height);
 _CFShared.CGAppearance = CGAppearance;
 _CFShared.CGCursor = CGCursor;
 _CFShared.CGFontManager = CGFontManager;
